8b4dbe8eab7558ca43edf06fd714f95a0cb73b2b
AbduGad <abdo79gad@gmail.com>
2023-11-28 21:37:12 +0200

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * @brief 
+ * 
+ * @param parent 
+ * @param value 
+ * @return binary_tree_t* 
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode, *temp;
+	
+	if(!parent)
+		return(NULL);
+	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if(!newNode)
+		return(NULL);
+		newNode->n = value;
+		newNode->left = newNode->right = NULL;
+	if(parent->left)
+	{
+		temp = parent->left;
+		parent->left = newNode;
+		newNode->left = temp;
+	}
+	else
+		parent->left = newNode;
+	return(newNode);
+}
