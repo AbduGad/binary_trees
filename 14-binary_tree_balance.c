380857965f72ee959f05ccde4f6b3cd9a2209faf
AbduGad <abdo79gad@gmail.com>
2023-11-30 00:15:22 +0200

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 /**
  * @brief 
  * 
@@ -7,7 +8,30 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int r = 0, l = 0;
+
 	if (!tree)
 		return (0);
-	return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+	l = binary_tree_height(tree->left);
+	printf("%i\n", l);
+	r = binary_tree_height(tree->right);
+	printf("%i\n", r);
+	return (l - r);
+}
+/**
+ * @brief 
+ * 
+ * @param tree 
+ * @return size_t 
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	left = right = 0;
+	if (!tree)
+		return (0);
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left > right ? left : right);
 }
