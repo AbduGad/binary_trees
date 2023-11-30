6df018ea409ed6026a0f488e30c3eb43bc2afcb3
AbduGad <abdo79gad@gmail.com>
2023-11-30 18:52:17 +0200

    test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,7 +22,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	/*if (!binary_tree_balance(tree))*/
 		/*return (1);*/
-	/*return (0);*/
+	return (0);
 }
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
