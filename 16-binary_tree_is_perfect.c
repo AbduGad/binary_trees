bfb8fc54269b038e951efcdc952b70fa5ae3e157
AbduGad <abdo79gad@gmail.com>
2023-11-30 18:48:13 +0200

    test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,10 +11,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (tree->left && tree->right)
 	{
-		binary_tree_is_perfect(tree->left);
-		binary_tree_is_perfect(tree->right);
+		if (!binary_tree_is_perfect(tree->left))
+			return (0);
+		if (!binary_tree_is_perfect(tree->right))
+			return (0);
 	}
-	else 
+	else if (!tree->left && !tree->right)
+		return (1);
+	else
 		return (0);
 	if (!binary_tree_balance(tree))
 		return (1);
