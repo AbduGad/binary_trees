d217d937dd875e72855693c73b7d54f6d251a0cd
AbduGad <abdo79gad@gmail.com>
2023-11-30 18:13:50 +0200

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->left || !tree->right)
+	if (tree->left || tree->right)
 		nodes += 1;
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
